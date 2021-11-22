3304f62c4f2ff9baf3ca9d5e6566bbc5c193867c
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-22 12:39:36 -0500

    this commit adds iss and index files which fetches ip address

---
+++ index.js
@@ -0,0 +1,20 @@
+const { fetchMyIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   let answer = JSON.parse(ip.body).ip;
+//   console.log('It worked! Returned IP:' , answer);
+//   return answer;
+// });
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
