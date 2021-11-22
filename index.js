a4b05adaef0c44dbd422fb9ce8514cc2837bde36
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2021-11-22 14:09:27 -0500

    cleanup index

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   let answer = JSON.parse(ip.body).ip;
-//   console.log('It worked! Returned IP:' , answer);
-//   return answer;
-// });
-
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
